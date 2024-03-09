ac13d4a2798f5fea2d1a9e51a82cca8b76b0acf5
sssseungk <cacy0118@naver.com>
2024-03-09 18:18:50 +0900

    💄 Header 컴포넌트 수정 및 스타일링

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,11 @@
+import './Header.css';
+
 function Header() {
   return (
-    <div>Header</div>
+    <div className='Header'>
+      <h3>오늘은 🐱</h3>
+      <h1>{new Date().toDateString()}</h1>
+    </div>
   )
 }
 
